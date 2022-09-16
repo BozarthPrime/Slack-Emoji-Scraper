5730ba69f2cc8d4c5806e422fed53812dd356055
wintersnow5174 <67990565+wintersnow5174@users.noreply.github.com>
2022-09-16 11:04:21 -0500

    Update slack-emoji-scraper.js
    Fixed Header File

--- slack-emoji-scraper.js
+++ slack-emoji-scraper.js
@@ -1,16 +1,26 @@
 /*
-Get Emoji Stats directly from Chrome developer tools with this script. Here's how:
-get emoji data
+# Slack-Emoji-Scraper
+Pulls emoji information from a slack instance
+
+## Usage
+Get Emoji Stats directly from your browser's developer tools with this script. Here's how:
+
 1. Open up https://yourdomain.slack.com/customize/emoji in a browser window
-2. Open up the Chrome Dev tools
-3. Go to the Sources Tab and the "Snippets" sub-tab (You may need to click the 3 dots next to Page or Filesystem to reveal)
-4. Create New Snippet and paste this code into there (Optional: Right click on the snippet name and rename it to something cool!)
-5. Run by right clicking on the Snippet name and selecting "Run"
-6. This will output all the emoji to the console
-7. Save output, formatting in bbedit or excel required
-Authors: Joseph B, Alysha L, Chad V
-Emoji JSON is:
-name,is_alias,alias_for,url,created,team_id,user_id,user_display_name,avatar_hash,can_delete,is_bad,synonyms
+2. Open up the developer console in your browser (genrally F12 but you can also use the menu)
+
+### For Firefox
+4. Click the "console" tab on the top of the tools bar
+5. Paste in the code (you may need to type an allow message for Firefox to allow this, it will let you know)
+6. Run the script by clicking on the run button in the top left corner of the dev tools window
+
+### For Chrome
+4. Go to the Sources Tab and the "Snippets" sub-tab (You may need to click the 3 dots next to Page or Filesystem to reveal)
+5. Create New Snippet and paste this code into there (Optional: Right click on the snippet name and rename it to something cool!)
+6. Run by right clicking on the Snippet name and selecting "Run"
+
+### Save the output
+7. This will output all the emoji to the console
+8. Save output, formatting in bbedit or excel required
 */
 
 function formatDate(date) {
